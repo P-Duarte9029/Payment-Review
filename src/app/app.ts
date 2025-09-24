b7cc588eaeb71ce0fe2558c50f3a6c10462a14aa
Pedro Duarte <pedroduarte9029@icloud.com>
2025-09-23 23:37:03 -0300

    atualização de tamplate

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InsertionArea } from './components/insertion-area/insertion-area';
 import { Header } from './components/header/header';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InsertionArea, Header],
+  imports: [RouterOutlet, InsertionArea, Header, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/insertion-area/insertion-area.ts
+++ src/app/components/insertion-area/insertion-area.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-insertion-area',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './insertion-area.html',
   styleUrl: './insertion-area.css'
 })
-export class InsertionArea {
-  nome:string = "Pedro";
+export class InsertionArea { 
+  valor:number = 120.10; 
+  valueArray: ValueData[] = [];
+  show_pop_up:boolean = false;
+
+  newInfo:string = "";
+  newValue:number = 0;
+
+  showP(){
+    this.show_pop_up = true;
+  }
+  closeP(){
+    this.show_pop_up = false;
+  }
+
+  SaveData(){
+    let newItem = new ValueData(this.newInfo, this.newValue);
+    this.valueArray.push(newItem);
+    this.closeP();
+    this.newInfo = "";
+    this.newValue = 0;
+  }
+
 }
+
+export class ValueData{
+     info: string;
+     value: number;
+     active: boolean;
+
+    constructor(info: string, value: number){
+      this.info = info;
+      this.value = value;
+      this.active = true;
+    }
+  }
