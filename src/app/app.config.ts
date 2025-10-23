9d527f0bd7cded7c967a2e8b5653973609b6a4b3
P-Duarte9029 <pedroduarte9029@icloud.com>
2025-10-23 13:02:45 -0300

    adição ao firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,11 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../enviroments/enviroments';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,7 +19,11 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideEnvironmentNgxMask()
+    provideEnvironmentNgxMask(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
 
   ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
